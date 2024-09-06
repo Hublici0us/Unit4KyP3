84e9ba475e9eba28f19e07c6cb6d42fca14adee2
Jason Ky <744169@student.dusd.net>
2024-09-06 11:47:05 -0700

    yeah
    I did it lets go

--- Unit4GameplayMechsKyP3/Assets/Scripts/SpawnManager.cs
+++ Unit4GameplayMechsKyP3/Assets/Scripts/SpawnManager.cs
@@ -1,15 +1,17 @@
-using System.Collections;
+
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public List<GameObject> enemyPrefab;
+    public List<GameObject> bossPrefab;
     public List<GameObject> powerUpPrefabs;
 
     private float spawnPos = 9.0f;
     public int enemyCount;
     public int waveCount = 1;
+    public int pastBossWaves = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +29,14 @@ public class SpawnManager : MonoBehaviour
         if (enemyCount == 0 )
         {
             waveCount++;
-            SpawnEnemyWave(waveCount);
+            if (waveCount == pastBossWaves * 5)
+            {
+                SpawnBossWave();
+            }
+            else
+            {
+                SpawnEnemyWave(waveCount);
+            }
             SpawnPowerUp();
         }
     }
@@ -53,8 +62,25 @@ public class SpawnManager : MonoBehaviour
     {
         for (int i = 0; i < enemiesToSpawn; i++)
         {
-            int chooseEnemy = Random.Range(0, enemyPrefab.Count);
+            int chooseEnemy;
+            float enemyDifficulty = (waveCount * 1.25f);
+            if (enemyDifficulty < enemyPrefab.Count)
+            {
+                chooseEnemy = Random.Range(0, Mathf.FloorToInt(enemyDifficulty));
+            }
+            else
+            {
+                chooseEnemy = Random.Range(0, enemyPrefab.Count);
+            }
             Instantiate(enemyPrefab[chooseEnemy], CreateRandomSpawn(), Quaternion.identity);
+
+            
         }
+    }
+
+    void SpawnBossWave()
+    {
+        int chooseBoss = Random.Range(0, bossPrefab.Count);
+        Instantiate(bossPrefab[chooseBoss], CreateRandomSpawn(), Quaternion.identity);
     }
 }
